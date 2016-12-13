df37577a0d5ec09f1d0a1d3f3cc4d0e36b9946b3
alanmoore <alan.moore@incapturetechnologies.com>
2016-12-13 09:00:25 -0800

    Adding program

---
+++ src/data/program.js
@@ -0,0 +1,26 @@
+module.exports = (config) => {
+  var module = {};
+  const gcloud = require('google-cloud');
+  const ds = gcloud.datastore(config);
+
+  console.log("datahub-common, program, Config was " + JSON.stringify(config));
+
+  module.createProgram = (program, cb) => {
+    var query = ds.createQuery('Program');
+    query.filter('id', program.id);
+    ds.runQuery(query, (err, programs) => {
+      if (err) {
+        return cb(err);
+      }
+      if (programs.length != 0) {
+        return cb('Program already exists');
+      }
+      ds.save({
+        key: ds.key(['Program', program.id]),
+        data: program
+      }, cb);
+    });
+  };
+
+  return module;
+}
