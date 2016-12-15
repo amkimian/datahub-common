ded0dfe9e70d43054389faa12afe7987814e8aee
alanmoore <alan.moore@incapturetechnologies.com>
2016-12-14 16:41:44 -0800

    Fix in config

--- src/index.js
+++ src/index.js
@@ -1,33 +1,32 @@
 module.exports = () => {
-  const config = require('./config');
   // Bind config
   var module = {};
+  module.config = require('./data/config.js');
 
   module.dataset = (projectId) => {
-    return require('./data/dataset.js')(config.getConfig(projectId));
+    return require('./data/dataset.js')(module.config.getConfig(projectId));
   };
 
   module.repo = (projectId) => {
-    return require('./data/repo.js')(config.getConfig(projectId));
+    return require('./data/repo.js')(module.config.getConfig(projectId));
   }
 
   module.subscription = (projectId) => {
-    return require('./data/subscription.js')(config.getConfig(projectId));
+    return require('./data/subscription.js')(module.config.getConfig(projectId));
   };
 
   module.user = (projectId) => {
-    return require('./data/user.js')(config.getConfig(projectId));
+    return require('./data/user.js')(module.config.getConfig(projectId));
   };
 
   module.program = (projectId) => {
-    return require('./data/program.js')(config.getConfig(projectId));
+    return require('./data/program.js')(module.config.getConfig(projectId));
   };
 
   module.pub = (projectId) => {
-    return require('./msg/pub.js')(config.getConfig(projectId));
+    return require('./msg/pub.js')(module.config.getConfig(projectId));
   };
 
-  module.config = require('./data/config');
 
   return module;
 }
