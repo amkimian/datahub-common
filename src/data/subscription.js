edcfe2ea26c001b20accedc1137eb7bc7b2fad25
Alan Moore <ukmoore@gmail.com>
2016-12-19 19:07:25 +0000

    Adding check subscription call

--- src/data/subscription.js
+++ src/data/subscription.js
@@ -1,24 +1,37 @@
 module.exports = (config) => {
-  var module = {};
-  const gcloud = require('google-cloud');
-  const ds = gcloud.datastore(config);
-  const Subscription = 'Subscription';
+	var module = {};
+	const gcloud = require('google-cloud');
+	const ds = gcloud.datastore(config);
+	const Subscription = 'Subscription';
 
-  module.getSubscriptions = (usercode, cb) => {
-    var query = ds.createQuery(Subscription);
-    query.filter('usercode', usercode);
-    ds.runQuery(query, cb);
-  };
+	module.getSubscriptions = (usercode, cb) => {
+		var query = ds.createQuery(Subscription);
+		query.filter('usercode', usercode);
+		ds.runQuery(query, cb);
+	};
 
-  module.createSubscription = (sub, cb) => {
-    // Create a new subscription
-    ds.save( {
-      key: ds.key([Subscription]),
-      data: sub
-    }, (err) => {
-      cb(err);
-    });
-  };
+	module.createSubscription = (sub, cb) => {
+		// Create a new subscription
+		ds.save({
+			key: ds.key([Subscription]),
+			data: sub
+		}, (err) => {
+			cb(err);
+		});
+	};
 
-  return module;
+	module.hasSubscription = (usercode, repocode, cb) => {
+		var query = ds.createQuery(Subscription);
+		query.filter('usercode', usercode);
+		query.filter('repocode', repocode);
+		ds.runQuery(query, (err, results) => {
+			if (err) {
+				return cb(err);
+			}
+			else {
+				return cb(null, results && results.length > 0);
+			}
+		});
+	}
+	return module;
 }
